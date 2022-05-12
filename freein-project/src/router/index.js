79f5237f98cb65b6651c32b5982e01b88bd64beb
Jacopo202 <jcapiato@gmail.com>
2022-05-12 15:30:33 +0200

    jacopo

--- freein-project/src/router/index.js
+++ freein-project/src/router/index.js
@@ -1,14 +1,14 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import FirstPage from '../views/FirstPage.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: HomeView
+    name: 'FirstPage',
+    component: FirstPage
   },
   {
     path: '/about',
---
+++ freein-project/src/views/FirstPage.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <FirstPageLayout/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import FirstPageLayout from '@/components/FirstPageLayout.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    FirstPageLayout
+  }
+}
+</script>
