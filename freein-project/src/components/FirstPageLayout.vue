11a63ee3b48dfd38e5816b79af5725f131064301
Jacopo202 <jcapiato@gmail.com>
2022-05-12 15:54:30 +0200

    Bootstrap

--- freein-project/src/components/FirstPageLayout.vue
+++ freein-project/src/components/FirstPageLayout.vue
@@ -1,5 +1,6 @@
 <template>
-<div> 
-    ciao
-</div>
+<div class="bg-image"
+                 style="background-image: url('https://images.hdqwalls.com/wallpapers/simple-minimal-mountains-landscape-4k-rc.jpg '); height: 100vh ">
+                
+            </div>
 </template>
