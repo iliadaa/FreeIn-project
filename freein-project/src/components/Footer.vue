1a42a057eaffd851f15c1b5ae1b3dd7c05407abc
iliadaa <iliadaarapi1@gmail.com>
2022-05-13 09:07:15 +0200

    add

--- freein-project/src/components/Footer.vue
+++ freein-project/src/components/Footer.vue
@@ -1,51 +1,27 @@
 <template>
-
-    <footer class="bg-dark text-center text-white S">
-        <!-- Grid container -->
-        <div class="container p-4 pb-0">
-            <!-- Section: Social media -->
-            <section class="mb-4">
-                <!-- Facebook -->
-                <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button">
-                    <i class="fab fa-facebook-f"></i>
-                </a>
-
-
-                <!-- Twitter -->
-                <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button">
-                    <i class="fab fa-twitter"></i>
-                </a>
-
-                <!-- Google -->
-                <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button">
-                    <i class="fab fa-google"></i>
-                </a>
-
-                <!-- Instagram -->
-                <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button">
-                    <i class="fab fa-instagram"></i>
-                </a>
-
-                <!-- Linkedin -->
-                <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button">
-                    <i class="fab fa-linkedin-in"></i>
-                </a>
-
-                <!-- Github -->
-                <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button">
-                    <i class="fab fa-github"></i>
-                </a>
-            </section>
-            <!-- Section: Social media -->
-        </div>
-        <!-- Grid container -->
-        <!-- Copyright -->
-        <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+<footer>
+<div class="d-flex bd-highlight">
+     <div class="p-2 flex-fill bd-highlight"   > <img src="logo-Freein.png"></div>
+     <div class="p-2 flex-fill bd-highlight">Information
+        <ul>
+            <li><a>Privacy notice</a></li>
+            <li><a>Conditions of Use</a></li>
+            <li><a>About us</a></li>
+        </ul>
+     </div>
+     <div class="p-2 flex-fill bd-highlight">User service </div>
+     <div class="p-2 flex-fill bd-highlight">My account</div>
+      <div class="p-2 flex-fill bd-highlight">Follow us <p> Free-in e un marchio di Ada Travl srl Via Punzi 101/D, 70024 Gravina in Puglia BA</p></div>
+    
+</div>
+<div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
           © 2020 Copyright:
-            <a class="text-white" href="">Iliada Arapi</a>
-        </div>
-        <!-- Copyright -->
-    </footer>
+            <a class="text-white" href="">Free In</a>
+ </div>
+
+</footer>
+ 
+  
 </template>
 
 
@@ -61,3 +37,28 @@ export default ({
         text-align: center;
     }
 </style>
+ <!-- Grid container -->
+        <div class="container p-4 pb-0">
+            <!-- Section: Social media -->
+            <section class="mb-4">
+                <!-- Facebook -->
+                <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button">
+                    <i class="fab fa-facebook-f"></i>
+                </a>
+                <!-- Twitter -->
+                <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button">
+                    <i class="fab fa-twitter"></i>
+                </a>
+
+                
+
+            </section>
+            <!-- Section: Social media -->
+        </div>
+        <!-- Grid container -->
+        <!-- Copyright -->
+        <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+          © 2020 Copyright:
+            <a class="text-white" href="">Iliada Arapi</a>
+        </div>
+        <!-- Copyright -->
