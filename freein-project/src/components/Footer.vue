cb7e107e48ae3e781ff18dca6eba18521067cc90
iliadaa <iliadaarapi1@gmail.com>
2022-05-13 10:46:48 +0200

    add

--- freein-project/src/components/Footer.vue
+++ freein-project/src/components/Footer.vue
@@ -1,44 +1,69 @@
 <template>
-<footer>
-<div class="d-flex bd-highlight">
-     <div class="p-2 flex-fill bd-highlight"   > <img src="logo-Freein.png"></div>
-     <div class="p-2 flex-fill bd-highlight">Information
+  <footer>
+    <div class="d-flex">
+      <div class="p-2 flex-fill">
+        <img class="logo" src="logo-Freein.png" />
+      </div>
+      <div class="p-2 flex-fill text-black">
+        <h5 class="text-center">Information</h5>
         <ul>
-            <li><a>Privacy notice</a></li>
-            <li><a>Conditions of Use</a></li>
-            <li><a>About us</a></li>
+          <li><a>Privacy notice</a></li>
+          <li><a>Conditions of Use</a></li>
+          <li><a>About us</a></li>
         </ul>
-     </div>
-     <div class="p-2 flex-fill bd-highlight">User service </div>
-     <div class="p-2 flex-fill bd-highlight">My account</div>
-      <div class="p-2 flex-fill bd-highlight">Follow us <p> Free-in e un marchio di Ada Travl srl Via Punzi 101/D, 70024 Gravina in Puglia BA</p></div>
-    
-</div>
-<div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
-          © 2020 Copyright:
-            <a class="text-white" href="">Free In</a>
- </div>
-
-</footer>
- 
-  
+      </div>
+      <div class="p-2 flex-fill bd-highlight text-black">
+        <h5 class="text-center">User Service</h5>
+      </div>
+      <div class="p-2 flex-fill bd-highlight text-black">
+        <h5 class="text-center">My account</h5>
+      </div>
+      <div class="p-2 flex-fill bd-highlight text-black">
+        <h5 class="text-center">Follow Us</h5>
+        <dl>
+          <dd>
+            Free-in e un marchio di Ada Travl srl Via Punzi 101/D, 70024 Gravina in Puglia
+            BA
+          </dd>
+          <dd>Numero REA BA - 608876</dd>
+          <dd>Partita iva 08161930725</dd>
+          <dd>Mail: contact@freein.it</dd>
+        </dl>
+        <div class="text-left">
+          <i class="fab fa-facebook-square fa-2x"></i>
+          <i class="fab fa-twitter-square fa-2x"></i>
+          <i class="fas fa-rss-square fa-2x"></i>
+        </div>
+      </div>
+    </div>
+    <div class="text-center p-3 text-black" style="background-color: rgba(0, 0, 0, 0.2)">
+      Copyright © 2022:
+      <a class="text-white" href="">Free In</a>
+      <div class="text-black">
+        Powered by
+        <a class="text-white" href="https://www.euronet.aero/cms"> CMS</a>
+      </div>
+    </div>
+  </footer>
 </template>
 
-
-
 <script>
-export default ({
-        name: 'Footer'
-})
+export default {
+  name: "Footer",
+};
 </script>
 
 <style scoped>
-    footer {
-        text-align: center;
-    }
+.flex-fill {
+  width: 20%;
+}
+.logo {
+  max-width: 60%;
+}
 </style>
- <!-- Grid container -->
-        <div class="container p-4 pb-0">
+
+<!-- Grid container -->
+<div class="container p-4 pb-0">
             <!-- Section: Social media -->
             <section class="mb-4">
                 <!-- Facebook -->
@@ -55,10 +80,10 @@ export default ({
             </section>
             <!-- Section: Social media -->
         </div>
-        <!-- Grid container -->
-        <!-- Copyright -->
-        <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+<!-- Grid container -->
+<!-- Copyright -->
+<div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2)">
           © 2020 Copyright:
             <a class="text-white" href="">Iliada Arapi</a>
         </div>
-        <!-- Copyright -->
+<!-- Copyright -->
