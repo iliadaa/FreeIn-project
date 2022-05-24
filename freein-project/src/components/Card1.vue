28084932b16e3fdf1d9b97f5b074e12f67c9365f
Jacopo202 <jcapiato@gmail.com>
2022-05-24 10:50:55 +0200

    First card and login 1

--- freein-project/src/components/Card1.vue
+++ freein-project/src/components/Card1.vue
@@ -1,11 +1,19 @@
 <template>
 <div class="container">
   <div class="card card-b">
-    <p>ciao</p>
+    <div class="card card-shadow1">
+      <p>ispira</p>
+      </div>
+    
+    
   </div>
 
   <div class="card card-r">
-    <p>ciao</p>
+    <div class="card card-shadow2">
+    <p>lasciati</p>
+    
+    <p style="margin-left:14px; margin-bottom:15px">ispirare</p>
+  </div>
   </div>
 </div>
 </template>
@@ -26,10 +34,36 @@ display: flex;
 height: 600px;
 width: 40%;
 background-color: #442a63;
+justify-content: center;
+align-items: center;
 }
 
 .card-r{
   background-color: #eb6349;
+}
+
+.card-b p{
+  color: #eb6349;
+  font-size: 100px; 
+}
+
+.card-r p{
+  color: #442a63;
+  font-size: 100px;
+  margin-bottom: -70px;
+}
+
+.card-shadow1{
+  box-shadow: 2px 2px 30px  rgba(25, 25, 25, 0.668);
+  width: 100%;
+  height: 100%;
+}
+
+.card-shadow2{
+  box-shadow: 2px 2px 30px  rgba(25, 25, 25, 0.668);
+  width: 100%;
+  height: 100%;
+  background-color: transparent;
 }
 
 
--- freein-project/src/components/Login.vue
+++ freein-project/src/components/Login.vue
@@ -1,19 +1,77 @@
 <template>
 <div class="container">
-  <div class="column">
-      <b-card header="Default" class="text-center">
-        <b-card-text>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</b-card-text>
-      </b-card>
+  <div class="card card-b">
+    <div class="card card-shadow1">
+      <p>ispira</p>
+      </div>
+    
+    
+  </div>
+
+  <div class="card card-r">
+    <div class="card card-shadow2">
+    <p>lasciati</p>
+    
+    <p style="margin-left:14px; margin-bottom:15px">ispirare</p>
   </div>
   </div>
-
-
-
+</div>
 </template>
 
+<script>
+
+</script>
+
 <style scoped>
+.container{
+margin-top: 100px;
+align-content: center;
+justify-content: center;
+display: flex;
+}
+
+.card{
+height: 600px;
+width: 40%;
+background-color:white;
+justify-content: center;
+align-items: center;
+}
+
+.card-r{
+  background-color: #ea910ce1;
+}
+
+.card-b p{
+  color: #eb6349;
+  font-size: 100px; 
+}
+
+.card-r p{
+  color: #442a63;
+  font-size: 100px;
+  margin-bottom: -70px;
+}
+
+.card-shadow1{
+  box-shadow: 2px 2px 30px  rgba(25, 25, 25, 0.668);
+  width: 100%;
+  height: 100%;
+}
+
+.card-shadow2{
+  box-shadow: 2px 2px 30px  rgba(25, 25, 25, 0.668);
+  width: 100%;
+  height: 100%;
+  background-color: transparent;
+}
+
 
 </style>
+
+
+
+
    <!--    
     <b-form @submit="onSubmit" @reset="onReset" v-if="show">
       <b-input-group class="userName" prepend="@">
