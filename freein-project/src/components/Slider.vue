a8352ca416ce069f69714195f9f8f795d49886ea
iliadaa <iliadaarapi1@gmail.com>
2022-05-25 12:23:48 +0200

    add

--- freein-project/src/components/Slider.vue
+++ freein-project/src/components/Slider.vue
@@ -1,32 +1,36 @@
 <template>
- <slick ref="slick" :options="slickOptions">
-  
-</slick>
+  <slick ref="slick" :options="slickOptions"> </slick>
 </template>
 
 <script>
-import Slick from 'vue-slick';
+import Slick from "vue-slick";
 
-export default({
-   components: { Slick },
-   data() {
-     return{
-      slickOptions:{
-        slidesToShow:3,
-        infinite:true,
-        adaptiveHeight: false,
-        arrows:true,
-        dots:true,
-        draggable:true,
-        edgeFriction:0.30,
-        swipe:true
-      }
-    }
- },
- methods:{
-  next() { this.$refs.slick.next()},
-  prev() {this.$refs.slick.prev()},
-  reInit() {this.$refs.slick.reSlick()}}
- })
+export default {
+  name: "Slider",
+  components: { Slick },
+  return: {
+    slickOptions: {
+      slidesToShow: 3,
+      infinite: true,
+      adaptiveHeight: false,
+      arrows: true,
+      dots: true,
+      draggable: true,
+      edgeFriction: 0.3,
+      swipe: true,
+    },
+  },
+  methods: {
+    next() {
+      this.$refs.slick.next();
+    },
+    prev() {
+      this.$refs.slick.prev();
+    },
+    reInit() {
+      this.$refs.slick.reSlick();
+    },
+  },
+};
 </script>
 <style></style>
--- freein-project/src/views/FirstPage.vue
+++ freein-project/src/views/FirstPage.vue
@@ -1,32 +1,32 @@
 <template>
   <div class="home">
+    <Header />
     <Slider />
-    <CardLayout/>
-    <CardLogin/>
-    <CardRegister/>
+    <CardLayout />
+    <CardLogin />
+    <CardRegister />
     <Footer />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
+import Header from "../components/Header.vue";
 import Slider from "../components/Slider.vue";
 import Footer from "../components/Footer.vue";
 import CardLayout from "../components/Cards/CardLayout.vue";
 import CardLogin from "../components/Cards/CardLogin.vue";
-
-import CardRegister from "../components/Cards/CardRegister.vue"
-
+import CardRegister from "../components/Cards/CardRegister.vue";
 
 export default {
   name: "HomeView",
   components: {
+    Header,
     Slider,
     Footer,
     CardLayout,
-    CardLogin,  
-    CardRegister,  
+    CardLogin,
+    CardRegister,
   },
 };
-
 </script>
