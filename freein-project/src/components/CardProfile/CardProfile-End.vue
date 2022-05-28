ca8bf11013418d6ad50e0b2f63d5753f6f3dac51
lela-lela <99544165+lela-lela@users.noreply.github.com>
2022-05-28 13:46:48 +0200

    fix style

--- freein-project/src/components/CardProfile/CardProfile-End.vue
+++ freein-project/src/components/CardProfile/CardProfile-End.vue
@@ -1,29 +1,175 @@
 <template>
 <div>
-  <h1>Complimenti ! <br> Sei un viaggiatore...</h1>
-  <img src="@/assets/photos.png" alt="" class="responsive" style="width: 100%; height: auto;">
-        <div class="target">
-            <a href="http://localhost:8080/#/" class="fas fa-bullseye"> </a>
-            <p >Vai alla tua Hall</p>
+  <h1>Complimenti! <br> Sei un viaggiatore...</h1>
+    <div class="cards">
+      <div class="arte">
+        <div class="image1">
+          <img src="@/assets/Arte-&-Cultura.png" alt="">
+        </div>
+        <div class="cardcontainer1">
+          <p>Arte & Cultura</p> 
+        </div>
+      </div>
+      <div class="mare">
+        <div class="image1">
+          <img src="@/assets/Mare.png" alt="">
+        </div>
+        <div class="cardcontainer2">
+          <p>Mare</p> 
+        </div>
+      </div>
+      <div class="gourmet">
+        <div class="image2">
+          <img src="@/assets/Gourmet-Explorer.png" alt="">
+        </div>
+        <div class="cardcontainer1">
+          <p>Gourmet Explorer</p> 
+        </div>
+      </div>
+      <div class="relax">
+        <div class="image3">
+          <img src="@/assets/Relax.png" alt="">
+        </div>
+        <div class="cardcontainer2">
+          <p>Relax</p> 
+        </div>
+      </div>
+      <div class="party">
+        <div class="image4">
+          <img src="@/assets/Party.png" alt="">
+        </div>
+        <div class="cardcontainer2">
+          <p>Party</p> 
+        </div>
+      </div>
+      <div class="natura">
+        <div class="image5">
+          <img src="@/assets/Natura-&-Avventura.png" alt="">
+        </div>
+        <div class="cardcontainer1">
+          <p>Natura & Avventura</p> 
+        </div>
+      </div>
+    </div>
+
+  <!--<img src="@/assets/photos.png" alt="">-->
+
+        <div class="target target_hall">
+            <a href="http://localhost:8080/#/" class="fas fa-bullseye"></a>
+            <div><p >Vai alla tua Hall</p></div> 
         </div>
 </div>
 </template>
 
-<style scoped>
-.logo {
-    margin-left: 10%;
-}
+<style>
 h1 {
-    color: #2d2e83;
-    font-size: 60px;
-    text-align: right;
-    margin-bottom: 10px;
-    margin-right: 10%;
+  color: #2d2e83;
+  font-size: 60px;
+  text-align: right;
 }
+
+.cards {
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+  height: auto;
+  margin-top: 70px;
+  margin-bottom: 70px;
+}
+
+.arte {
+  background-color: #EC624C;
+  width: 160px;
+  height: 220px;
+  position: relative;
+  box-shadow: 2px 2px 30px rgba(25, 25, 25, 0.668);
+}
+
+.mare {
+  background-color: #2C307F;
+  width: 160px;
+  height: 220px;
+  position: relative;
+  box-shadow: 2px 2px 30px rgba(25, 25, 25, 0.668);
+}
+
+.gourmet {
+  background-color: #009EE2;
+  width: 160px;
+  height: 220px;
+  position: relative;
+  box-shadow: 2px 2px 30px rgba(25, 25, 25, 0.668);
+}
+
+.relax {
+  background-color: #DC2C6C;
+  width: 160px;
+  height: 220px;
+  position: relative;
+  box-shadow: 2px 2px 30px rgba(25, 25, 25, 0.668);
+}
+
+.party {
+  background-color: purple;
+  width: 160px;
+  height: 220px;
+  position: relative;
+  box-shadow: 2px 2px 30px rgba(25, 25, 25, 0.668);
+}
+
+.natura {
+  background-color: #F6A314;
+  width: 160px;
+  height: 220px;
+  position: relative;
+  box-shadow: 2px 2px 30px rgba(25, 25, 25, 0.668);
+}
+
+.image1 {
+  padding: 10px;
+}
+
+.image2 {
+  padding-left: 20px;
+}
+
+.image3 {
+  padding-left: 7px;
+}
+
+.image4 {
+  padding-left: 20px;
+}
+
+.image5 {
+  padding-left: 10px;
+}
+
+.cardcontainer1 {
+  font-size: 30px;
+  font-weight: bold;
+  color: white;
+  text-align: center;
+  position: absolute;
+  line-height: 22px;
+  top: 170px;
+}
+
+.cardcontainer2 {
+  font-size: 30px;
+  font-weight: bold;
+  color: white;
+  position: absolute;
+  line-height: 22px;
+  top: 193px;
+  left: 45px;
+}
+
 .target {
-text-align: right;
-margin-right: 10%;
+  text-align: right;
+  margin-right: 5px;
 }
+
 .target a {
   justify-content: center;
   align-items: center;
@@ -35,8 +181,12 @@ margin-right: 10%;
   text-decoration: none;
   color: white;
   background-image: linear-gradient(to bottom right, #ea5b0c, #2d2e83); 
-  margin-right: 10px;
   margin-bottom: 10px;
 }
-
+.target_hall a {   
+  margin-right: 25px;
+}
 </style>
+
+
+
