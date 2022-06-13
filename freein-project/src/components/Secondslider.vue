0893a9ba74da012b472fe0271a07c0b46cbb4b4e
lela-lela <99544165+lela-lela@users.noreply.github.com>
2022-06-13 21:51:55 +0200

    new

--- freein-project/src/components/Secondslider.vue
+++ freein-project/src/components/Secondslider.vue
@@ -5,31 +5,32 @@
     </div>
     
     <div>
-        <p class="c">
-            Freein il primo vero
-            Social travel
-            che ti premia.
-        </p>
+            <div class="heading">
+                <p>Freein il primo vero </p>
+                <p>Social travel</p>
+                <p>che ti premia.</p>
+            </div>
+            
+            <div class="text1">
+                <p>Freein ti premia</p>
+            </div>
     
-        <p class="c">
-            Freein ti premia
-        </p>
-    
-        <p class="c">
-            Crea e condividi il tuo itinerario e
-            guadagnerai ogni volta che i tuoi amici lo
-            salveranno
-        </p>
-    
-        <p class="c">
-            Potrai spendere i tuoi crediti scegliendo
-            le tappe che ti ispirano di piu'
-        </p>
-    
-        <p class="c">
-            Se sei un'azienda turistica potrai utilizzare
-            i tuoi crediti sponsorizzando la tua attivita'
-        </p>
+        
+            <div class="text2">
+                <p>Crea e condividi il tuo itinerario e</p>
+                <p>guadagnerai ogni volta che i tuoi amici lo</p>
+                <p>salveranno</p>
+            </div>
+        
+            <div class="text3">
+                <p>Potrai spendere i tuoi crediti scegliendo</p>
+                <p>le tappe che ti ispirano di piu'</p>
+            </div>
+        
+            <div class="text4">
+                <p>Se sei un'azienda turistica potrai utilizzare</p>
+                <p>i tuoi crediti sponsorizzando la tua attivita'</p>
+            </div>      
     </div>
 </div>
 </template>
@@ -38,10 +39,50 @@
 .container {
     display: flex;
     justify-content: space-between;
+    margin-top: 40px;
 }
-p.c {
-    white-space: pre;
+
+img {
+    margin-top: 150px;
+    margin-left: 100px;
 }
+p {
+    text-align: right;
+    color: #ea5b0c;
+    margin-right: 100px;
+}
+.heading {
+    font-size: 50px;
+    font-weight: bold;
+    line-height: 40px;
+}
+
+.text1 {
+    font-size: 25px;
+    line-height: 15px;
+    margin-top: 30px;
+}
+
+.text2 {
+    font-size: 25px;
+    line-height: 15px;
+    margin-top: 40px;
+}
+
+.text3 {
+    font-size: 25px;
+    line-height: 15px;
+    margin-top: 40px;
+}
+
+.text4 {
+    font-size: 25px;
+    line-height: 15px;
+    margin-top: 40px;
+}
+
+
+
 </style>
 
 
