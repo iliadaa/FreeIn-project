5889dcf6236bcf085f07510ceadc2b8169027ed5
lela-lela <99544165+lela-lela@users.noreply.github.com>
2022-06-13 17:35:09 +0200

    changes

--- freein-project/src/components/Secondslider.vue
+++ freein-project/src/components/Secondslider.vue
@@ -1,17 +1,50 @@
 <template>
-<div>
-    <img src="@/assets/girls.png" alt="">
-</div>
-<div class="heading">
-    <p>Freein il primo vero</p>
-    <p>Social Travel</p>
-    <p>che ti premia.</p>
+<div class="container">
+    <div>
+        <img src="@/assets/girls.png" alt="">
+    </div>
+    
+    <div>
+        <p class="c">
+            Freein il primo vero
+            Social travel
+            che ti premia.
+        </p>
+    
+        <p class="c">
+            Freein ti premia
+        </p>
+    
+        <p class="c">
+            Crea e condividi il tuo itinerario e
+            guadagnerai ogni volta che i tuoi amici lo
+            salveranno
+        </p>
+    
+        <p class="c">
+            Potrai spendere i tuoi crediti scegliendo
+            le tappe che ti ispirano di piu'
+        </p>
+    
+        <p class="c">
+            Se sei un'azienda turistica potrai utilizzare
+            i tuoi crediti sponsorizzando la tua attivita'
+        </p>
+    </div>
 </div>
 </template>
 
 <style scoped>
-
+.container {
+    display: flex;
+    justify-content: space-between;
+}
+p.c {
+    white-space: pre;
+}
 </style>
+
+
 <script>
 export default{
     name:"Secondslider",
