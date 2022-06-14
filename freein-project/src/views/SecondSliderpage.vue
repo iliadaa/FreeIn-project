c10711e93b7fbedb31224d777a610b2f5320809e
iliadaa <iliadaarapi1@gmail.com>
2022-06-14 16:50:11 +0200

    Update SecondSliderpage.vue

--- freein-project/src/views/SecondSliderpage.vue
+++ freein-project/src/views/SecondSliderpage.vue
@@ -1,12 +1,29 @@
 <template>
-<div>
-  <!--<Secondslider/>-->
- <Thirdslider/>
-</div></template>
+  <div>
+    <div class="container"><Header /></div>
+    <div class="first-slider">
+      <img src="../assets/eiffel.jpg" />
+    </div>
+    <Secondslider />
+    <Thirdslider />
+  </div>
+</template>
 <script>
-import Secondslider from '@/components/Secondslider.vue'
-import Thirdslider from '@/components/Thirdslider.vue'
+import Secondslider from "@/components/Secondslider.vue";
+import Thirdslider from "@/components/Thirdslider.vue";
+import Header from "@/components/Header.vue";
 export default {
-components:{Secondslider, Thirdslider 
-}};
+  name: "SecondSliderpage",
+  components: { Secondslider, Thirdslider, Header },
+};
 </script>
+
+<style>
+.SecondSliderpage .navbar {
+  position: absolute;
+  z-index: 1;
+  top: 0;
+  left: 10%;
+  padding-top: 0;
+}
+</style>
