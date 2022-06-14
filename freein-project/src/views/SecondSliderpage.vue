e0bcbfc49a355f5335daeebe723c68d1577efb36
lela-lela <99544165+lela-lela@users.noreply.github.com>
2022-06-14 18:33:27 +0200

    changes

--- freein-project/src/views/SecondSliderpage.vue
+++ freein-project/src/views/SecondSliderpage.vue
@@ -1,8 +1,24 @@
 <template>
   <div class="SecondSliderpage">
     <div class="container"><Header /></div>
+
     <div class="first-slider">
       <img src="../assets/eiffel.jpg" />
+      <div class="first-slider-caption">
+        <p>Viaggia</p>
+        <p>Ispira &</p>
+        <p>Lasciati ispirare.</p>
+
+        <div class="discover">
+            <div>
+              <a href="#">Scopri come</a>
+            </div>
+            <div>
+              <a href="#"><i class="fas fa-angle-double-right"></i></a>
+            </div>
+          
+        </div>
+      </div>
     </div>
     <Secondslider />
     <Thirdslider />
@@ -27,9 +43,53 @@ export default {
   padding-top: 0;
 }
 
-.first-slider img{
+.first-slider {
+  position: relative;
+  z-index: 100;
+}
+
+.first-slider img {
   width: 100%;
 }
+
+.first-slider-caption {
+  position: absolute;
+  text-align: left;
+  top: 20%;
+  left: 15%;
+  bottom: auto;
+}
+
+.first-slider-caption p {
+  font-size: 60px;
+  font-weight: bold;
+  color: white;
+  line-height: 50px;
+}
+
+.discover {
+  display: flex;
+  margin-left: 305px;
+  margin-top: 50px;
+}
+
+.first-slider-caption a {
+  text-decoration: none;
+  color: white;
+  font-size: 20px;
+}
+
+.first-slider-caption a:hover {
+  color: white;
+}
+
+.first-slider-caption i {
+  color: white;
+  font-size: 25px;
+  padding-top: 5px;
+  padding-left: 10px;
+}
+
 
 
 
