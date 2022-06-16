de6fc9d4c3dc917e7270d791be72f43d335fabd2
lela-lela <99544165+lela-lela@users.noreply.github.com>
2022-06-16 17:17:52 +0200

    changes

--- freein-project/src/components/Users Cards/DialogPage-Card.vue
+++ freein-project/src/components/Users Cards/DialogPage-Card.vue
@@ -2,14 +2,17 @@
 <div class="container">
     <div class="dialogpage1">
         <div class="dialogpage-links">
-            <p><a href="https://templates.ener-web.it/freeintest/">E-commerce</a></p>
+            <p><a href="https://templates.ener-web.it/freeintest/" target="iframe_a">E-commerce</a></p>
             <p><a href="https://www.contattipower.it/" target="iframe_a">Marketing</a></p>
             <p><a href="https://signin.travelappeal.com/#/login?redirect=)" target="iframe_a">Benchmark</a></p>
             <p><a href="https://app.tanaza.com/signin" target="iframe_a">Wi-Fi</a></p>
+            <img src="@/assets/dots.png" alt="">
         </div>
 
         <div class="dialogpage-iframe">
-            <iframe src="demo_iframe.htm" name="iframe_a" height="600px" width="800px" title="Iframe"></iframe>
+            <iframe  seamless="seamless" scrolling="no" src="_blank" name="iframe_a" height="600px" width="800px" title="Iframe"></iframe>
+
+            <hr>
         </div>
     </div>
 </div>
@@ -21,6 +24,29 @@
     justify-content: space-evenly;
     margin-top: 40px;
     margin-bottom: 100px;
+}
+
+.dialogpage-links {
+    background-color: #ea5b0c;
+    border-radius: 25px 0px 0px 25px; 
+    height: 600px;
+    width: 170px;
+}
+
+.dialogpage-links p {
+    font-size: 18px;
+    padding-top: 30px;
+    padding-left: 20px;
+} 
+
+.dialogpage-links a {
+    color: white;
+    text-decoration: none;    
+}
+
+.dialogpage-links img {
+    padding-top: 253px;
+    padding-left: 5px;
 }
 
 </style>
