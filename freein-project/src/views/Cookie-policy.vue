18c6610aa4167c1c06bc2697f43df78d415d8ef4
lela-lela <99544165+lela-lela@users.noreply.github.com>
2022-06-17 15:39:49 +0200

    new

--- freein-project/src/views/Cookie-policy.vue
+++ freein-project/src/views/Cookie-policy.vue
@@ -1,3 +1,201 @@
 <template>
-  <div>Lelaaaaaaaaaaaaaaa</div>
+  <div class="container">
+
+<h1>cookie policy</h1> 
+
+<p><strong>Cookie Policy</strong> – <a href="/privacy-policy">Privacy Policy</a></p>
+
+<p>I Cookie sono costituiti da porzioni di codice installate all’interno del browser che assistono il Titolare nell’erogazione del Servizio in base alle finalità descritte. Alcune delle finalità di installazione dei Cookie potrebbero, inoltre, necessitare del consenso dell’Utente.</p>
+
+<p>Quando l’installazione di Cookies avviene sulla base del consenso, tale consenso può essere revocato liberamente in ogni momento seguendo le istruzioni contenute in questo documento.</p>
+
+<h2>Definizioni e riferimenti legali</h2>
+
+<h3>Dati Personali (o Dati)</h3>
+
+<p>Costituisce dato personale qualunque informazione che, direttamente o indirettamente, anche in collegamento con qualsiasi altra informazione, ivi compreso un numero di identificazione personale, renda identificata o identificabile una persona fisica.</p>
+
+<h3>Dati di Utilizzo</h3>
+
+<p>Sono le informazioni raccolte automaticamente attraverso questo Sito Web (anche da applicazioni di parti terze integrate in questo Sito Web), tra cui: gli indirizzi IP o i nomi a dominio dei computer utilizzati dall’Utente che si connette con questo Sito Web, gli indirizzi in notazione URI (Uniform Resource Identifier), l’orario della richiesta, il metodo utilizzato nell’inoltrare la richiesta al server, la dimensione del file ottenuto in risposta, il codice numerico indicante lo stato della risposta dal server (buon fine, errore, ecc.) il paese di provenienza, le caratteristiche del browser e del sistema operativo utilizzati dal visitatore, le varie connotazioni temporali della visita (ad esempio il tempo di permanenza su ciascuna pagina) e i dettagli relativi all’itinerario seguito all’interno dell’Applicazione, con particolare riferimento alla sequenza delle pagine consultate, ai parametri relativi al sistema operativo e all’ambiente informatico dell’Utente.</p>
+
+<h3>Utente</h3>
+
+<p>L’individuo che utilizza questo Sito Web che, salvo ove diversamente specificato, coincide con l’Interessato.</p>
+
+<h3>Interessato</h3>
+
+<p>La persona fisica cui si riferiscono i Dati Personali.</p>
+
+<h3>Responsabile del Trattamento (o Responsabile)</h3>
+
+<p>La persona fisica, giuridica, la pubblica amministrazione e qualsiasi altro ente che tratta dati personali per conto del Titolare, secondo quanto esposto nella privacy policy.</p>
+
+<h3>Titolare del Trattamento (o Titolare)</h3>
+
+<p>La persona fisica o giuridica, l’autorità pubblica, il servizio o altro organismo che, singolarmente o insieme ad altri, determina le finalità e i mezzi del trattamento di dati personali e gli strumenti adottati, ivi comprese le misure di sicurezza relative al funzionamento ed alla fruizione di questo Sito Web. Il Titolare del Trattamento, salvo quanto diversamente specificato, è il titolare di questo Sito Web.</p>
+
+<h3>Questo Sito Web (o questa Applicazione)</h3>
+
+<p>Lo strumento hardware o software mediante il quale sono raccolti e trattati i Dati Personali degli Utenti.</p>
+
+<h3>Servizio</h3>
+
+<p>Il Servizio fornito da questo Sito Web così come definito nei relativi termini (se presenti) su questo sito/applicazione.</p>
+
+<h3>Unione Europea (o UE)</h3>
+
+<p>Salvo ove diversamente specificato, ogni riferimento all’Unione Europea contenuto in questo documento si intende esteso a tutti gli attuali stati membri dell’Unione Europea e dello Spazio Economico Europeo.</p>
+
+<h3>Cookie</h3>
+
+<p>Piccola porzione di dati conservata all’interno del dispositivo dell’Utente.</p>
+
+<h3>Riferimenti legali</h3>
+
+<p>L’informativa privacy è redatta sulla base di molteplici ordinamenti legislativi, inclusi gli artt. 13 e 14 del Regolamento (UE) 2016/679.</p>
+
+<p>Ove non diversamente specificato, questa informativa privacy riguarda esclusivamente questo Sito Web.</p>
+
+<h2>Cookie tecnici e di statistica aggregata</h2>
+
+<h3>Attività strettamente necessarie al funzionamento</h3>
+
+<p>Questo Sito Web utilizza Cookie per salvare la sessione dell’Utente e per svolgere altre attività strettamente necessarie al funzionamento di questo Sito Web, ad esempio in relazione alla distribuzione del traffico.</p>
+
+<h3>Attività di salvataggio delle preferenze, ottimizzazione e statistica</h3>
+
+<p>Questo Sito Web utilizza Cookie per salvare le preferenze di navigazione ed ottimizzare l’esperienza di navigazione dell’Utente. Fra questi Cookie rientrano, ad esempio, quelli per impostare la lingua e la valuta o per la gestione di statistiche da parte del Titolare del sito.</p>
+
+<h2>Altre tipologie di Cookie o strumenti terzi che potrebbero installarne</h2>
+
+<p>Alcuni dei servizi elencati di seguito raccolgono statistiche in forma aggregata ed anonima e potrebbero non richiedere il consenso dell’Utente o potrebbero essere gestiti direttamente dal Titolare – a seconda di quanto descritto – senza l’ausilio di terzi.</p>
+
+<p>Qualora fra gli strumenti indicati in seguito fossero presenti servizi gestiti da terzi, questi potrebbero – in aggiunta a quanto specificato ed anche all’insaputa del Titolare – compiere attività di tracciamento dell’Utente. Per informazioni dettagliate in merito, si consiglia di consultare le privacy policy dei servizi elencati.</p>
+
+<h3>Interazione con le piattaforme di live chat</h3>
+
+<p>Questo tipo di servizi permette di interagire con le piattaforme di live chat, gestite da soggetti terzi, direttamente dalle pagine di questo Sito Web. Ciò permette all’Utente di contattare il servizio di supporto di questo Sito Web o a questo Sito Web di contattare l’Utente mentre sta navigando le sue pagine. <br>
+Nel caso in cui sia installato un servizio di interazione con le piattaforme di live chat, è possibile che, anche nel caso gli Utenti non utilizzino il servizio, lo stesso raccolga Dati di Utilizzo relativi alle pagine in cui è installato. Inoltre, le conversazioni della live chat potrebbero essere registrate.
+</p>
+
+<h3>Interazione con social network e piattaforme esterne</h3>
+
+<p>Questo tipo di servizi permette di effettuare interazioni con i social network, o con altre piattaforme esterne, direttamente dalle pagine di questo Sito Web.
+Le interazioni e le informazioni acquisite da questo Sito Web sono in ogni caso soggette alle impostazioni privacy dell’Utente relative ad ogni social network.
+Nel caso in cui sia installato un servizio di interazione con i social network, è possibile che, anche nel caso gli Utenti non utilizzino il servizio, lo stesso raccolga dati di traffico relativi alle pagine in cui è installato.
+</p>
+
+<h3><strong>Pulsante Mi Piace e widget sociali di Facebook (Facebook, Inc.)</strong></h3>
+
+<p>Il pulsante “Mi Piace” e i widget sociali di Facebook sono servizi di interazione con il social network Facebook, forniti da Facebook, Inc.</p>
+<p>Dati Personali raccolti: Cookie e Dati di utilizzo. </p>
+<p>Luogo del trattamento: USA – <a href="https://www.facebook.com/privacy/explanation" target="_blank" rel="noopener noreferrer">Privacy Policy.</a></p>
+
+<h3><strong>Pulsante “Pin it” e widget sociali di Pinterest (Pinterest)</strong></h3>
+
+<p>Il pulsante “Pin it” e widget sociali di Pinterest sono servizi di interazione con la piattaforma Pinterest, forniti da Pinterest Inc.</p>
+
+<p>Dati Personali raccolti: Cookie e Dati di utilizzo.</p>
+
+<p>Luogo del trattamento: USA – <a href="https://about.pinterest.com/en/privacy-policy" target="_blank" rel="noopener noreferrer">Privacy Policy.</a></p>
+
+<h3><strong>Pulsante Tweet e widget sociali di Twitter (Twitter, Inc.)</strong></h3>
+
+<p>Il pulsante Tweet e i widget sociali di Twitter sono servizi di interazione con il social network Twitter, forniti da Twitter, Inc.</p>
+
+<p>Dati Personali raccolti: Cookie e Dati di utilizzo.</p>
+
+<p>Luogo del trattamento: USA – <a href="https://twitter.com/privacy" target="_blank" rel="noopener noreferrer">Privacy Policy.</a></p>
+
+<h3>Statistica</h3>
+
+<p>I servizi contenuti nella presente sezione permettono al Titolare del Trattamento di monitorare e analizzare i dati di traffico e servono a tener traccia del comportamento dell’Utente.</p>
+
+<h3><strong>Google Analytics (Google Inc.)</strong></h3>
+
+<p>Google Analytics è un servizio di analisi web fornito da Google Inc. (“Google”). Google utilizza i Dati Personali, <span>raccolti in forma anonima su questo sito</span>, allo scopo di tracciare ed esaminare l’utilizzo di questo Sito Web, compilare report e condividerli con gli altri servizi sviluppati da Google.
+Google potrebbe utilizzare i Dati Personali, <span>raccolti in modo anonimo su questo sito</span> per contestualizzare e personalizzare gli annunci del proprio network pubblicitario.</p>
+
+<p>Dati Personali raccolti: Cookie e Dati di utilizzo.</p>
+
+<p>Luogo del trattamento: USA – Privacy Policy – Opt Out.</p>
+
+<h3>Visualizzazione di contenuti da piattaforme esterne</h3>
+
+<p>Questo tipo di servizi permette di visualizzare contenuti ospitati su piattaforme esterne direttamente dalle pagine di questo Sito Web e di interagire con essi.
+Nel caso in cui sia installato un servizio di questo tipo, è possibile che, anche nel caso gli Utenti non utilizzino il servizio, lo stesso raccolga dati di traffico relativi alle pagine in cui è installato.</p>
+
+<h3><strong>Widget Video YouTube (Google Inc.)</strong></h3>
+
+<p>YouTube è un servizio di visualizzazione di contenuti video gestito da Google Inc. che permette a questo Sito Web di integrare tali contenuti all’interno delle proprie pagine.</p>
+
+<p>Dati Personali raccolti: Cookie e Dati di utilizzo.</p>
+
+<p>Luogo del trattamento: Stati Uniti – <a href="http://www.google.it/intl/it/policies/privacy/" target="_blank" rel="noopener noreferrer">Privacy Policy.</a></p>
+
+<h3><strong>Come posso esprimere il consenso all’installazione di Cookie?</strong></h3>
+
+<p>In aggiunta a quanto indicato in questo documento, l’Utente può gestire le preferenze relative ai Cookie direttamente all’interno del proprio browser ed impedire – ad esempio – che terze parti possano installarne. Tramite le preferenze del browser è inoltre possibile eliminare i Cookie installati in passato, incluso il Cookie in cui venga eventualmente salvato il consenso all’installazione di Cookie da parte di questo sito. L’Utente può trovare informazioni su come gestire i Cookie con alcuni dei browser più diffusi ad esempio ai seguenti indirizzi: <a href="https://support.google.com/chrome/answer/95647?hl=it&amp;p=cpn_cookies" target="_blank" rel="noopener">Google Chrome</a>, <a href="https://support.mozilla.org/it/kb/Attivare%20e%20disattivare%20i%20cookie" target="_blank" rel="noopener">Mozilla Firefox</a>, <a href="https://support.apple.com/kb/PH21411" target="_blank" rel="noopener">Apple Safari</a> e <a href="http://windows.microsoft.com/it-it/windows-vista/block-or-allow-cookies" target="_blank" rel="noopener">Microsoft Internet Explorer</a>.</p>
+
+<p>Con riferimento a Cookie installati da terze parti, l’Utente può inoltre gestire le proprie impostazioni e revocare il consenso visitando il relativo link di opt out (qualora disponibile), utilizzando gli strumenti descritti nella privacy policy della terza parte o contattando direttamente la stessa.</p>
+
+<p>Fermo restando quanto precede, l’Utente può avvalersi delle informazioni fornite da <a href="http://www.youronlinechoices.eu/" target="_blank" rel="noopener">EDAA</a> (UE), <a href="https://www.networkadvertising.org/understanding-digital-advertising" target="_blank" rel="noopener">Network Advertising Initiative</a> (USA) e <a href="https://www.aboutads.info/consumers/" target="_blank" rel="noopener">Digital Advertising Alliance</a> (USA), <a href="https://youradchoices.ca/understanding-online-advertising/" target="_blank" rel="noopener">DAAC</a> (Canada), <a href="http://www.ddai.info/optout" target="_blank" rel="noopener">DDAI</a> (Giappone) o altri servizi analoghi. Con questi servizi è possibile gestire le preferenze di tracciamento della maggior parte degli strumenti pubblicitari. Il Titolare, pertanto, consiglia agli Utenti di utilizzare tali risorse in aggiunta alle informazioni fornite dal presente documento.</p>
+
+<h3><strong>Titolare del Trattamento dei Dati</strong></h3>
+
+<p>Antonio Prota – Via Punzi, 101/D – 70024 Gravina in Puglia (Ba) – Italia</p>
+
+<p><strong>Indirizzo email del Titolare:</strong> info@adatravel.it</p>
+
+<p><strong>Per qualsiasi chiarimento,</strong> informazione, <br>
+esercizio dei diritti elencati nella presente informativa, <br>
+contattare il titolare alla seguente mail: <strong>info@adatravel.it</strong></p>
+
+<p>Dal momento che l’installazione di Cookie e di altri sistemi di tracciamento operata da terze parti tramite i servizi utilizzati all’interno di questo Sito Web non può essere tecnicamente controllata dal Titolare, ogni riferimento specifico a Cookie e sistemi di tracciamento installati da terze parti è da considerarsi indicativo. Per ottenere informazioni complete, l’Utente è invitato a consultare la privacy policy degli eventuali servizi terzi elencati in questo documento.</p>
+
+<p>Vista l’oggettiva complessità di identificazione delle tecnologie basate sui Cookie l’Utente è invitato a contattare il Titolare qualora volesse ricevere qualunque approfondimento relativo all’utilizzo dei Cookie stessi tramite questo Sito Web.</p>
+
+  </div>
 </template>
+
+<style scoped>
+.container {
+  margin-top: 40px;
+  margin-bottom: 100px;
+}
+
+h1 {
+  font-size: 65px;
+  font-weight: bold;
+  letter-spacing: -1px;
+  color: #4b4948;
+  text-align: center;
+}
+
+h2 {
+  font-size: 36px;
+  font-weight: bold;
+  margin-bottom: 20px;
+}
+
+h4 {
+  font-size: 28px;
+  font-weight: bold;
+  margin-bottom: 30px;
+}
+
+h3 {
+  font-size: 32px;
+  margin-bottom: 30px;
+}
+
+a {
+  text-decoration: none;
+  color: red;
+}
+
+span {
+  text-decoration: underline;
+}
+</style>
