cdfd93219da863782c2aa72f0891d6ca30be95b2
Jacopo202 <jcapiato@gmail.com>
2022-09-15 10:13:36 +0200

    Travis added

--- freein-project/deploy.sh
+++ freein-project/deploy.sh
@@ -15,6 +15,7 @@ cd dist
 git init
 git add -A
 git commit -m 'deploy'
+git push -f git@github.com:iliadaa/FreeIn-project.git master
 
 # if you are deploying to https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
